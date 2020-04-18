1787ea603eb2b1cc580e7c343a46ab7eac60a8e3
rafaelmoura29 <rafaelmoura2929@gmail.com>
2020-04-18 17:15:55 -0300

    Criando rotas post e get cliente

--- vendas/src/main/java/io/github/rafaelmoura29/VendasApplication.java
+++ vendas/src/main/java/io/github/rafaelmoura29/VendasApplication.java
@@ -1,47 +1,15 @@
 package io.github.rafaelmoura29;
 
 import io.github.rafaelmoura29.domain.entity.Cliente;
-import io.github.rafaelmoura29.domain.entity.Pedido;
 import io.github.rafaelmoura29.domain.repositories.Clientes;
-import io.github.rafaelmoura29.domain.repositories.Pedidos;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import java.math.BigDecimal;
-import java.time.LocalDate;
-import java.util.List;
-
 @SpringBootApplication
 public class VendasApplication {
-
-    @Bean
-    public CommandLineRunner init(
-            @Autowired Clientes clientes,
-            @Autowired Pedidos pedidos
-    ){
-        return args -> {
-            System.out.println("Salvando Cliente");
-            clientes.save( new Cliente( "Rafael"));
-            Cliente fulano = new Cliente( "Outro Cliente");
-            clientes.save(fulano);
-
-            Pedido p = new Pedido();
-            p.setCliente(fulano);
-            p.setDataPedido(LocalDate.now());
-            p.setTotal(BigDecimal.valueOf(100));
-
-            pedidos.save(p);
-
-            pedidos.findByCliente(fulano).forEach(System.out::println);
-
-           /* Cliente cliente = clientes.findClienteFetchPedidos(fulano.getId());
-            System.out.println(cliente);
-            System.out.println(cliente.getPedidos());*/
-        };
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class, args);
